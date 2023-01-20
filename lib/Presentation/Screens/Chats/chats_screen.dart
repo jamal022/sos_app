3e367ded6e7fcdc0a3f55a3de9e825618e1266e8
ClaraWilliam1311 <clara.william.140@gmail.com>
2023-01-20 13:11:14 +0200

    second commit

--- lib/Presentation/Screens/Chats/chats_screen.dart
+++ lib/Presentation/Screens/Chats/chats_screen.dart
@@ -8,7 +8,7 @@ class ChatsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Center(
-      child: Text("chats"),
+      child: Text("chats screen"),
     );
   }
 }
